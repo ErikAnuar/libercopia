49b53ceb2dfd8feb1a5b92b1d87fc065c2fd5d4e
Erik <erik02003@mail.ru>
2023-02-22 17:48:51 +0600

    first commit

---
+++ cmd/api/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"flag"
+	"os"
+	"sync"
+)
+
+const version = "1.0.0"
+
+type config struct {
+	port int
+	env  string
+	db   struct {
+		dsn          string
+		maxOpenConns int
+		maxIdleConns int
+		maxIdleTime  string
+	}
+	limiter struct {
+		rps     float64
+		burst   int
+		enabled bool
+	}
+	smtp struct {
+		host     string
+		port     int
+		username string
+		password string
+		sender   string
+	}
+}
+
+type application struct {
+	config config
+	logger *jsonlog.logger
+	models data.Models
+	mailer mailer.mailer
+	wg     sync.WaitGroup
+}
+
+func main() {
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+
+	flag.StringVar(&cfg.db.dsn, "db_dsn", os.Getenv("LIBERCOPIA_DSN"), "PostgreSQL DSN")
+	
+	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
+	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
+	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "PostgreSQL max connection idle time")
+
+	flag.Float64Var(&cfg.limiter.rps, "limiter-rps", 2, "Rate limiter maximum requests per second")
+	flag.IntVar(&cfg.limiter.burst, "limiter-burst", 4, "Rate limiter maximum burst")
+	flag.BoolVar(&cfg.limiter.enabled, "limiter-enabled", true, "Enable rate limitter")
+
+	flag.StringVar(&cfg.smtp.host, "smtp-host", "smtp-mail.outlook.com", "SMTP host")
+	flag.IntVar(&cfg.smtp.port, "smtp-port", 587, "SMTP port")
+	flag.StringVar(&cfg.smtp.username, "smtp-username", "libercopia@outlook.com", "SMTP username")
+	flag.StringVar(&cfg.smtp.password, "smtp-password", "Lzrbt95fU$VC3(W", "SMTP password")
+	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Libercopia <libercopia@outlook.com>", "SMTP sender")
+
+	flag.Parse()
+
+
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module libracopia
+
+go 1.19
